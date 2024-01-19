67588683693c45850370c3931228b43fe5bd2f24
Gursimar Virdi <385491@colum.edu>
2024-01-18 23:25:37 -0600

    Adding comments

--- lib/game.h
+++ lib/game.h
@@ -35,25 +35,34 @@ protected:
     }
 };
 
-class AssignmentGame : public BaseGame {
+class AssignmentGame : public BaseGame{
 protected:
-    int solve() override {
+    int solve() override
+    {
         int hits = 0;
 
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                if (this->board->guess(i, j) == ResponseType::HIT) {
+        // Goes over each part of the board by position
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+                // Check if the guess at this position is a HIT
+                if (this->board->guess(i, j) == ResponseType::HIT)
+                {
+                    //Increase the hit counter
                     hits += 1;
                 }
 
-                if (this->board->guess(i, j) == ResponseType::NEARMISS) {
-                    // straight
+                // Check if the guess at this position is a NEARMISS
+                if (this->board->guess(i, j) == ResponseType::NEARMISS)
+                {
+                    // Check for nearby hits in straight directions
                     hits += TestNearMiss(i + 1, j);
                     hits += TestNearMiss(i - 1, j);
                     hits += TestNearMiss(i, j + 1);
                     hits += TestNearMiss(i, j - 1);
 
-                    // diagonal
+                    // Check for nearby hits in diagonal directions
                     hits += TestNearMiss(i + 1, j + 1);
                     hits += TestNearMiss(i - 1, j - 1);
                     hits += TestNearMiss(i - 1, j + 1);
@@ -61,18 +70,26 @@ protected:
                 }
             }
         }
-
+        // Return the total hits count on the board
         return hits;
     }
 
-    int TestNearMiss(int x, int y) {
-        if (CheckValidGuess(x, y) && this->board->guess(x, y) == ResponseType::HIT) {
+    // Function to test for a near miss at a given position
+    int TestNearMiss(int x, int y)
+    {
+        // Check if the guess at the specified position is a HIT and the position is valid
+        if (CheckValidGuess(x, y) && this->board->guess(x, y) == ResponseType::HIT)
+        {
+            // Return 1 if it's a near miss
             return 1;
         }
+        // Return 0 if not a near miss
         return 0;
     }
 
-    bool CheckValidGuess(int x, int y) {
+    // Function to check if a guess at the specified position is valid
+    bool CheckValidGuess(int x, int y)
+    {
         return x >= 0 && x < HEIGHT && y >= 0 && y < WIDTH;
     }
 };
